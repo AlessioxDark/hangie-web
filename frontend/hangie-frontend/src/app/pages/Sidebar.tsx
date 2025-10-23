42800b85b038e6e0fb10c621c77037b4212ce567
Alessio40 <alessio40aq@gmail.com>
2025-10-23 20:59:53 +0200

    finito refactoring sidebar

--- frontend/hangie-frontend/src/app/pages/Sidebar.tsx
+++ frontend/hangie-frontend/src/app/pages/Sidebar.tsx
@@ -1,12 +1,11 @@
-import { House, MessageCircle, Ticket, User } from 'lucide-react';
+import SidebarIcons from '@/utils/SidebarIcons';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../../assets/logo.svg';
+/** */
 type ValidPath = `/${string}`;
 type SidebarLinksType = {
 	id: number;
-	title: 'Home' | 'Chats' | 'Profilo' | 'Eventi';
-	icon: React.ReactNode;
+	title: 'Home' | 'Chats' | 'Profilo' | 'Amici';
 	link: ValidPath;
 	description: string;
 };
@@ -17,28 +16,25 @@ const Sidebar = () => {
 		{
 			id: 1,
 			title: 'Home',
-			icon: <House className="w-6 h-6" aria-hidden="true" />,
 			link: '/',
 			description: 'Scopri nuovi eventi',
 		},
 		{
 			id: 2,
 			title: 'Chats',
-			icon: <MessageCircle className="w-6 h-6" aria-hidden="true" />,
+
 			link: '/chats',
 			description: 'I tuoi messaggi',
 		},
 		{
 			id: 3,
-			title: 'Eventi',
-			icon: <Ticket className="w-6 h-6" aria-hidden="true" />,
-			link: '/events',
+			title: 'Amici',
+			link: '/Friends',
 			description: 'I tuoi eventi',
 		},
 		{
 			id: 4,
 			title: 'Profilo',
-			icon: <User className="w-6 h-6" aria-hidden="true" />,
 			link: '/profile',
 			description: 'Impostazioni account',
 		},
@@ -53,17 +49,18 @@ const Sidebar = () => {
 	};
 
 	return (
-		<aside className="h-full w-1/5 p-6 flex flex-col gap-8 shadow-xl bg-primary">
+		<aside className="xl:h-full xl:w-1/5 xl:p-6 flex flex-col gap-28 shadow-xl bg-bg-1">
 			<header className="flex w-full flex-row gap-4 items-center py-4">
-				<div className="p-2 rounded-2xl bg-white/10 backdrop-blur-sm">
-					<img
+				{/* <img
 						src={logo}
 						alt="Hangie Logo"
 						className="w-15 h-15"
 						loading="lazy"
-					/>
+					/> */}
+				<div className="bg-primary rounded-2xl py-4 px-6">
+					<span className="font-body text-bg-1 font-black text-6xl">H</span>
 				</div>
-				<h1 className="font-title font-bold text-3xl text-white">Hangie</h1>
+				<h1 className="font-body font-bold text-5xl text-text-1">Hangie</h1>
 			</header>
 
 			{/* Navigation */}
@@ -80,12 +77,13 @@ const Sidebar = () => {
 							to={link.link}
 							key={link.id}
 							className={`
-								flex items-center gap-4 p-4 rounded-xl transition-all duration-300 group text-white
+								flex items-center gap-3 px-3 py-4 rounded-xl transition-all duration-300 group font-medium
 								focus:outline-none focus:ring-2 focus:ring-white/50
+                
 								${
 									isActive
-										? 'shadow-lg transform scale-105 bg-accent'
-										: 'hover:transform hover:scale-105 hover:shadow-md bg-white/10 hover:bg-white/20'
+										? 'bg-[#D9EAFF] text-primary hover:bg-[#D9EAFF] '
+										: 'hover:transform  text-text-2 bg-bg-1 hover:bg-[#F1F4F8]'
 								} 
 							`}
 							aria-label={link.description}
@@ -96,9 +94,9 @@ const Sidebar = () => {
 									isActive ? 'scale-110' : 'group-hover:scale-110'
 								}`}
 							>
-								{link.icon}
+								<SidebarIcons isActive={isActive} title={link.title} />
 							</div>
-							<span className="font-title text-lg font-medium">
+							<span className="font-body font-medium text-xl">
 								{link.title}
 							</span>
 						</Link>
